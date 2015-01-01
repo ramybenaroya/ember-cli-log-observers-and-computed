545a12e9c551bbe269c3b9c80d03b4ca907c3575
ramyb <rami@dudamobile.com>
2015-01-01 15:10:25 +0200

    dummy app changes

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	aComputedProperty: function(){
 		return this.get('counter');
-	}.property('counter', 'some3', 'some1', 'some2'),
+	}.property('counter'),
 	anObserver: function(){
 		var aComputedProperty = this.get('aComputedProperty');
 		this.set('output', aComputedProperty);
-	}.observes('counter', 'some1'),
+	}.observes('counter'),
 	init: function(){
 		this._super();
 		setInterval(Ember.run.bind(this, this.incrementProperty, 'counter'), 500);
